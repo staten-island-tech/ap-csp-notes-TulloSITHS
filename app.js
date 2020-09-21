1acd936878036126dd0aac4f24408d484ceb03bd
TulloSITHS <50631839+TulloSITHS@users.noreply.github.com>
2020-09-21 11:09:44 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+//Use var to make a variable (variable used to hold data)
+
+//Syntax: var name = value;
+
+//; is used to dicate where a line of code ends
+
+//When assigning a word or a string to a variable you need to add quotes
+
+//Make sure to use a // to comment to the code
+
+//It is important to write yourself and others notes in your code so its clear what's going on
+
+//You can later change the value of variable or even do mathematical equations in the code
+
+//(ex: life = life - 10; life = life + 10; life = life / 10; life = life * 10)
+
+//Booleans are variables that only have a value of true or false
+
+//Booleans are useful especially in making if statements, if else statements, esc.
+
+//Null is used to state that a variable does not have a value
+
+//const is a constant variable, it cannot be modified or changed in the code later on
+
+//It also cannot be redeclared
+
+//let however lets you constant the value of the variable unlike const
