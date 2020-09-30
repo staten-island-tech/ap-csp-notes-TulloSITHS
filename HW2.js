563a1e36cc8301494faf0310ffbc4831124e9dca
TulloSITHS <50631839+TulloSITHS@users.noreply.github.com>
2020-09-30 10:15:30 -0400

    Add files via upload

---
+++ HW2.js
@@ -0,0 +1,25 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//Basic math operators
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2; 
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Objects
+val = Math.PI;
+val = Math.E;
+val = Math.round();
+val = Math.ceil();
+val = Math.floor();
+val = Math.sqrt();
+val = Math.abs();
+val = Math.pow();
+val = Math.min();
+val = Math.max();
+val = Math.random(); //random decimal 
+
+val = Math.floor(Math.random() * 20 + 1); //this gives a random whole number from 1 - 20
